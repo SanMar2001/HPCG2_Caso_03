faed71a262a5205b0bd059a4dcfa51548676eee1
SanMar2001 <s.martinez4@utp.edu.co>
2025-11-25 01:15:46 -0500

    test added

---
+++ mpi/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+> results_mpi.doc
+> results_seq.doc
+
+for size in 400 800 1200 1600; do
+    for n in 2 4 6 8; do
+        for rep in {1..10}; do
+            mpirun -f ../hosts.txt -n $n mpi_version $size >> "results_mpi.doc"
+        done
+    done
+
+    for rep in {1..10}; do
+        ../sequential $size >> "results_seq.doc"
+    done
+
+done
