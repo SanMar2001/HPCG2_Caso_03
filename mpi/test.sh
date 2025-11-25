ddfbc025978d4fe18159fc184c9c2df0c28ef843
SanMar2001 <s.martinez4@utp.edu.co>
2025-11-25 06:41:37 +0000

    tests added

--- mpi/test.sh
+++ mpi/test.sh
@@ -6,7 +6,7 @@
 for size in 400 800 1200 1600; do
     for n in 2 4 6 8; do
         for rep in {1..10}; do
-            mpirun -f ../hosts.txt -n $n mpi_version $size >> "results_mpi.doc"
+            mpirun -f ../hosts.txt -n $n ./mpi_version $size >> "results_mpi.doc"
         done
     done
 
